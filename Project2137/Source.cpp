483cc279e89a1ea786b30503ad14f4533e4e939a
franq200 <kuj-fran@wp.pl>
2023-02-20 18:24:51 +0100

    Init commit

---
+++ Project2137/Source.cpp
@@ -0,0 +1,30 @@
+
+int main()
+{
+	return 0;
+}
+
+/*
+Stworzyæ nowy projekt zgodnie z zasadami TDD. (musz¹ byæ testy, ka¿da funkcjonalnoœæ musi byæ otestowana) (failuj¹cy test->dzia³aj¹cy test->refactor)
+Projekt powinien byæ wersjonowany(GITHUB)
+StudentBase
+Baza danych studentów
+
+1. Dodanie nowego studenta
+2. Wyszukanie studenta(po peselu lub id lub nazwisku)->result->mo¿na wypisaæ(studenta mo¿na wyœwietliæ)
+3. Usuniêcie studenta
+4. Edycja danych studenta
+5. Export do Excela
+6. Wydruk do PDF
+Student
+{
+	id;
+	imie
+	nazwisko
+	data urodzenia
+	p³eæ
+	pesel; // co z obcokrajowcami?? (nie musisz mieæ ¿adnego zastêpczego pola)
+	narodowoœæ
+}
+
+*/
---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include "../Project2137/Base.h"
+
+TEST(TestCaseName, TestName) {
+	Base(1);
+}
